9699684d34de6e4c45c77761efe94e42febeb197
Josh <joshuafirme1@gmail.com>
2025-09-10 21:43:01 +0800

    update app v

--- core/app/Http/Helpers/helpers.php
+++ core/app/Http/Helpers/helpers.php
@@ -20,7 +20,7 @@ function systemDetails()
 {
     $system['name'] = 'GV Florida';
     $system['version'] = '2.0';
-    $system['build_version'] = '5.1.4';
+    $system['build_version'] = '5.1.5';
     return $system;
 }
 
