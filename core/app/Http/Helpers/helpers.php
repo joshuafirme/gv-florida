e443f23cea5d73f8da6f50d9dbe780cc2695a8bf
Josh <joshuafirme1@gmail.com>
2026-04-24 20:03:41 +0800

    Update helpers.php

--- core/app/Http/Helpers/helpers.php
+++ core/app/Http/Helpers/helpers.php
@@ -20,7 +20,7 @@ function systemDetails()
 {
     $system['name'] = 'GV Florida';
     $system['version'] = '2.0';
-    $system['build_version'] = '5.3.3';
+    $system['build_version'] = '5.3.4';
     return $system;
 }
 
